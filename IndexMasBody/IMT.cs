daf83258711b36b24bef654d4943ed477d93f477
ArtemPudov <artem.pudoff@yandex.ru>
2020-11-11 19:31:54 +0300

    add unit test

---
+++ IndexMasBody/IMT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndexMasBody
+{
+    public class IMT
+    {
+        public static string IMT1(double indexMB)
+        {
+            string s;
+            if (indexMB <= 16)
+            {
+                s = "ИМТ = "+indexMB.ToString()+" - выраженный дефицит массы тела.";
+            }
+            else if (indexMB <= 18.5)
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - недостаточная (дефицит)масса тела. ";
+            }
+            else if (indexMB <= 24.99)
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - норма ";
+            }
+            else if (indexMB <= 30)
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - избыточная масса тела(предожирение) ";
+            }
+            else if (indexMB <= 35)
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - ожирение первой степени ";
+            }
+            else if (indexMB <= 40)
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - ожирение второй степени ";
+            }
+            else
+            {
+                s = "ИМТ = " + indexMB.ToString() + " - ожирение третьей степени(морбидное) ";
+            }
+
+            return s;
+
+        }
+    }
+}
---
+++ IndexMasBodyTest/IndexMasBodyTest.cs
@@ -0,0 +1,86 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IndexMasBody;
+namespace IndexMasBodyTest
+{
+    
+    [TestClass]
+    public class IndexMasBodyTest
+    {
+        
+        [TestMethod]
+        public void T_to16()
+        {
+            double indexMB = 16;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = "+indexMB.ToString()+ " - выраженный дефицит массы тела.");
+
+        }
+
+
+        [TestMethod]
+        public void T_to18_5()
+        {
+            double indexMB = 18.5;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - недостаточная (дефицит)масса тела. ");
+
+        }
+
+        [TestMethod]
+        public void T_to24_99()
+        {
+            double indexMB = 24.99;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - норма ");
+
+        }
+
+        [TestMethod]
+        public void T_to30()
+        {
+            double indexMB = 30;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - избыточная масса тела(предожирение) ");
+
+        }
+
+        [TestMethod]
+        public void T_to35()
+        {
+            double indexMB = 35;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - ожирение первой степени ");
+
+        }
+
+        public void T_to40()
+        {
+            double indexMB = 40;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - ожирение второй степени ");
+
+        }
+        public void T_from40()
+        {
+            double indexMB = 41;
+
+            string s = IMT.IMT1(indexMB);
+
+            Assert.AreEqual(s, "ИМТ = " + indexMB.ToString() + " - ожирение третьей степени(морбидное) ");
+
+        }
+
+    }
+}
